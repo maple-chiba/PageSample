71ae1c54f556226e3f39b11b40d6bf26dcc50c49
Yuka Chiba <y.chiba@maplesystems.co.jp>
2015-07-17 15:30:43 +0900

    初回コミット

---
+++ PageSample/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  PageSample
+//
+//  Created by USER on 2015/06/18.
+//  Copyright (c) 2015年 YukaChiba. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
